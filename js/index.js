ec91d69a992a9b27a54ce89cca8683cc3348c271
selam lemma <oneiconforlife26@gmail.com>
2020-09-25 23:29:43 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 var name = "Joe"
 var height = "74"
+  if (name === "susan") {
+    console.log("The name is correct")
+  } else {
+    console.log("Expected the nnam to be susan")
+  }
 
 // Don't worry about this
 module.exports = { name, height
